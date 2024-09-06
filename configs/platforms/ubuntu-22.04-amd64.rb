5ac4e049c40b62e63f20f4be47fdcaf3030d218e
alex501212 <alexthepatterson@hotmail.com>
2024-09-06 12:20:26 +0100

    docker build for ubuntu 22.04

--- configs/platforms/ubuntu-22.04-amd64.rb
+++ configs/platforms/ubuntu-22.04-amd64.rb
@@ -1,5 +1,9 @@
 platform "ubuntu-22.04-amd64" do |plat|
   plat.inherit_from_default
+  plat.docker_run_args ['--tty', '--entrypoint=/bin/sh']
+  plat.docker_image "amd64/ubuntu:24.04"
+  plat.use_docker_exec true
+  
   packages = %w(git)
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
 end
