1cdf7c272f523c440cb015a770fffeb19ed5e006
alex501212 <alexthepatterson@hotmail.com>
2024-09-06 12:47:11 +0100

    changed 22.04 and 24.04

---
+++ configs/platforms/ubuntu-24.04-amd64.rb
@@ -0,0 +1,19 @@
+platform "ubuntu-22.04-amd64" do |plat|
+  plat.inherit_from_default
+
+  plat.docker_run_args ['--tty', '--entrypoint=/bin/sh']
+  plat.docker_image "amd64/ubuntu:24.04"
+  plat.use_docker_exec true
+  
+  packages = %w(
+    libbz2-dev
+    libreadline-dev
+    libselinux1-dev
+    gcc
+    swig
+    systemtap-sdt-dev
+    zlib1g-dev
+  )
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive && apt-get update -qq && apt-get install -qy --no-install-recommends #{packages.join(' ')}"
+  plat.provision_with "curl https://artifactory.delivery.puppetlabs.net/artifactory/api/gpg/key/public | apt-key add -"
+end
